bd77f30b85363007e9f0e7eb97d30669fb4c1afa
R-E-yawn <aryantyagi1117@gmail.com>
2024-09-07 20:13:44 -0400

    Add altairAcceptedEpochs.m MATLAB file

---
+++ altairAcceptedEpochs.m
@@ -0,0 +1,21 @@
+function [epochsAcceptedPercent] = altairAcceptedEpochs(EEG)
+% Extract the EEG, based on Gorka's code
+%
+%  Parameters:
+%     filename  EEGLAB EEG structure
+
+%   Output:
+%     EEG     EEGLAB EEG structure with the output
+%
+%   Written by:  John LaRocco, Feb 2016
+%
+% Assumes:  SOAR preprocessing base on Gorka for rough EEG.
+y1=EEG.ntrials.accepted;
+%epochAccepted=[epochAccepted; y1];
+y2=EEG.ntrials.rejected;
+%epochRejected=[epochRejected; y2];
+
+epochsTotal = y1+y2;
+epochsAcceptedPercent = y1/epochsTotal;
+
+end

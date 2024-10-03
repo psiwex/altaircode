ff3c85ce46ef4336450dd47105e899c448736e1b
psiwex <130997158+psiwex@users.noreply.github.com>
2024-10-03 09:13:35 -0400

    Add files via upload

--- EEG_quality_generator.m
+++ EEG_quality_generator.m
@@ -1,9 +1,9 @@
 classdef EEG_quality_generator
     % Class used to generate the quality statistics for ERN, LST and NPU
     % tests
-    % Version 1: 2024-09-17
-    % requires access to altairErnPreproc, altairNpuPreproc and altairLstPreproc
+    % Version 2: 2024-10-02 - should run on server
     % Written by:  Antoine Jean (jean.75@osu.edu / antoine.jean.2@ulaval.ca), 2024-09-11
+
     properties
         raw_bdf_loadpath = '';
         raw_dataset_savepath = '';
@@ -18,15 +18,20 @@ classdef EEG_quality_generator
         binlisterPathNpu = '';
         channelLocationFile = '';
 
+        uniq_locations = {};
         n_entries = 0;
         subject_names = {};
+        subject_locations = {};
+
         ern_qualities = [];
         lst_qualities = [];
-        npu_qualities = [];     
+        npu_qualities = [];    
+
+        save_processed = true;
     end
     
     methods
-        function obj = EEG_quality_generator(soarDataPath, savedDataPath, binlisters_path, channelLocationFile)
+        function obj = EEG_quality_generator(soarDataPath, savedDataPath, binlisters_path, channelLocationFile, save_processed)
             % When the class is created with a given soarDataPath (which should be a directory), it
             % automatically looks for folders in that directory. All folders found are assumed to be individuals (the name of the folder is the subject name) 
             % It will later look in those folder for specific entries:
@@ -48,9 +53,29 @@ classdef EEG_quality_generator
             obj.binlisterPathNpu = [binlisters_path, 'NpuBinlister.txt'];
             % channelLocationFile: this path is found in your eeglab dipfit plugin folder: eeglab\plugins\dipfit\standard_BESA\standard-10-5-cap385.elp
             obj.channelLocationFile = channelLocationFile;
+            obj.save_processed = save_processed;
 
-            % getting the subdirectory names
-            obj.subject_names = obj.getSubDirectories(soarDataPath);
+            % getting the subdirectory names - first level is the locations
+            obj.uniq_locations = obj.getSubDirectories(soarDataPath);
+
+            % get directory names - second level is the individuals 
+            % Only add the subject if all files are present
+            for i = 1:length(obj.uniq_locations)
+                loc = char(obj.uniq_locations(i));
+                individuals_at_location = obj.getSubDirectories([soarDataPath, loc]);
+                for j = 1:length(individuals_at_location)
+                    nam = char(individuals_at_location(j));
+                    if obj.subject_contains_all_files(nam, loc)
+                       % add the individual to the data
+                       % saves the name and the location for future use
+                       % doesn't save the path directly, it's reconstructed
+                       % later from location + name
+                       obj.subject_names{end + 1} = nam;
+                       obj.subject_locations{end + 1} = loc;
+                    end
+                end
+            end
+
             obj.n_entries = length(obj.subject_names);
 
             % initiating output arrays with 0s
@@ -60,11 +85,78 @@ classdef EEG_quality_generator
         end
         
         function obj = calculate_all_quality_scores(obj)
+            % runs preprocessing functions for all individuals 
             for i = 1:obj.n_entries
-                [ern, lst, npu] = obj.calculate_quality_scores(obj.subject_names(i));
+                [ern, lst, npu] = obj.calculate_quality_scores(i);
                 obj.ern_qualities(i) = ern;
                 obj.lst_qualities(i) = lst;
                 obj.npu_qualities(i) = npu;  
+            end
+        end
+
+        function [ern, lst, npu] = calculate_quality_scores(obj, subjectIndex)
+            subjectName = char(obj.subject_names(subjectIndex));
+            subjectlocation = char(obj.subject_locations(subjectIndex));
+            % we know the files already exist if the subject is in the list
+            [lstFileName, npuFileName, ernFileName] = obj.find_filenames(subjectName, subjectlocation);
+
+            % run preprocessing functions once the files are found
+            lst = -1;
+            % Requires signal processing toolbox
+            lst = obj.preproc(lstFileName, "LST", subjectName, subjectlocation);
+            npu = obj.preproc(npuFileName, "NPU", subjectName, subjectlocation);
+            ern = obj.preproc(ernFileName, "ERN", subjectName, subjectlocation); 
+
+        end
+
+
+        function [lstFileName, npuFileName, ernFileName] = find_filenames(obj, subjectName, subjectlocation)
+            % find the filenames of the bdf files to read given the index
+            filestem = [obj.raw_bdf_loadpath, '\', subjectlocation, '\', subjectName, '\'];
+            
+            % select path to look into, if Actiview doesn't exist we look
+            % in the original directory
+            path_containing_bdf_files = [filestem, 'Actiview\'];
+            if exist(path_containing_bdf_files, 'dir') ~= 7
+               path_containing_bdf_files = filestem;
+            end
+            
+            % get the bdf file names
+            [lstFileName, npuFileName, ernFileName] = obj.find_bdf_files_from_dir(path_containing_bdf_files);
+        end
+
+        function [lstFileName, npuFileName, ernFileName] = find_bdf_files_from_dir(obj, filename)
+            % assuming the filename already exists
+            lstFileName = obj.find_bdf_by_pattern(filename, "*LST.bdf");
+            npuFileName = obj.find_bdf_by_pattern(filename, "*NPU.bdf");
+            ernFileName = obj.find_bdf_by_pattern(filename, "*ERN.bdf");
+        end
+
+
+
+        function res = preproc(obj, filename, test_code, subjectName, subjectlocation)
+            % checks if the file exists. If it does, calculate the
+            % preprocessing function based on input string code, then saves
+            % the data if save_processed == true. If file doesn't exist,
+            % just returns -1, but this should not happen naturally
+            % (version 2)
+            if exist(filename, 'file') == 2
+                if test_code == "LST"
+                     [EEGe,~,~] = altairLstPreproc(filename, obj.binlisterPathLst, obj.channelLocationFile);
+                elseif test_code == "NPU"
+                     [EEGe,~,~] = altairNpuPreproc(filename,obj.raw_dataset_savepath,obj.ongoing_dataset_savepath,obj.erpset_savepath, obj.binlisterPathNpu, obj.channelLocationFile);
+                elseif  test_code == "ERN"
+                     [EEGe,~,~] = altairErnPreproc(filename,obj.raw_dataset_savepath,obj.ongoing_dataset_savepath,obj.erpset_savepath, obj.binlisterPathErn, obj.channelLocationFile);
+                end
+                [res] = altairAcceptedEpochs(EEGe);
+                
+                if obj.save_processed
+                    processed_file = [obj.processed_dataset_savepath, char(subjectlocation), '_', ...
+                        char(subjectName), '_' ,'Processed_', char(test_code)];
+                    save(processed_file,'EEGe');
+                end
+            else 
+                res = -1;
             end
         end
 
@@ -78,85 +170,37 @@ classdef EEG_quality_generator
             end
         end
 
-        function [ern, lst, npu] = calculate_quality_scores(obj, subjectName)
-            subjectName = char(subjectName);
-            filestem = [obj.raw_bdf_loadpath, subjectName, '\', subjectName];
-
-            lstFileName = [filestem, '-LST.bdf'];
-            lst = -1;
-            % Requires signal processing toolbox
-            % lst = obj.lstPreproc(lstFileName);
-
-            npuFileName = [filestem, '-NPU.bdf'];
-            npu = obj.npuPreproc(npuFileName);
-
-            ernFileName = [filestem, '-ERN.bdf'];
-            ern = obj.ernPreproc(ernFileName); 
-
-        end
-
-
-        function res = lstPreproc(obj, filename)
-            if exist(filename, 'file') == 2
-                [EEGe,~,~] = altairLstPreproc(filename, obj.binlisterPathLst, obj.channelLocationFile);
-                [res] = altairAcceptedEpochs(EEGe);
-            else 
-                res = -1;
-            end
-        end
-
-        function res = npuPreproc(obj, filename)
-            if exist(filename, 'file') == 2
-                [EEGe,~,~] = altairNpuPreproc(filename,obj.raw_dataset_savepath,obj.ongoing_dataset_savepath,obj.erpset_savepath, obj.binlisterPathNpu, obj.channelLocationFile);
-                [res] = altairAcceptedEpochs(EEGe);
-            else
-                res = -1;
-            end
-        end
-
-        % passes the processing function as an input to reuse code(works as long as all preprocessing functions have the same signature)
-        function res = ernPreproc(obj, filename)
-            if exist(filename, 'file') == 2
-                [EEGe,~,~] = altairErnPreproc(filename,obj.raw_dataset_savepath,obj.ongoing_dataset_savepath,obj.erpset_savepath, obj.binlisterPathErn, obj.channelLocationFile);
-                [res] = altairAcceptedEpochs(EEGe);
-            else
-                % if the file doesn't exist, the return value is -1 for now
-                res = -1;
-            end
-        end
 
         function saveEEGtoCSV(obj)
+            % saves quality percentages in csv along with id info for every
+            % subject ('subject_name', 'subject_location', 'ern_quality', 'lst_quality', 'npu_quality')
             cellArray = cell(obj.n_entries, 4);
             for i = 1:obj.n_entries
                 cellArray{i, 1} = obj.subject_names(i);
-                cellArray{i, 2} = obj.ern_qualities(i);
-                cellArray{i, 3} = obj.lst_qualities(i);
-                cellArray{i, 4} = obj.npu_qualities(i);
+                cellArray{i, 2} = obj.subject_locations(i);
+                cellArray{i, 3} = obj.ern_qualities(i);
+                cellArray{i, 4} = obj.lst_qualities(i);
+                cellArray{i, 5} = obj.npu_qualities(i);
             end
-            T = cell2table(cellArray, 'VariableNames', {'subject_name', 'ern_quality', 'lst_quality', 'npu_quality'});
+            T = cell2table(cellArray, 'VariableNames', {'subject_name', 'subject_location', 'ern_quality', 'lst_quality', 'npu_quality'});
             % Transpose so that all rows in table are records of one individual
             writetable(T,'eeg_quality_data.csv')
         end
         
-        % returns a boolean if the subject is correctly formatted in the
-        % file system
-        function res = subject_contains_all_files(obj, subjectName)
-            subjectName = char(subjectName);
-            filestem = [obj.raw_bdf_loadpath, subjectName, '\', subjectName];
-            ernFileName = [filestem, '-ERN.bdf'];
-            lstFileName = [filestem, '-LST.bdf'];
-            npuFileName = [filestem, '-NPU.bdf'];
-            
-            res = exist(ernFileName, 'file') == 2 && ...
-                exist(lstFileName, 'file') == 2 && ...
-                exist(npuFileName, 'file') == 2;
+        
+        function res = subject_contains_all_files(obj, subjectName, subjectlocation)
+            % returns a boolean if there are all 3 tests for subject name and location 
+            [lstFileName, npuFileName, ernFileName] = obj.find_filenames(subjectName, subjectlocation);
+            res = (~isempty(lstFileName) && ~isempty(npuFileName) && ~isempty(ernFileName));
         end
 
         function list_subjects(obj)
             for i = 1:obj.n_entries
-                subName = char(obj.subject_names(i));
-                is_valid = char(string(obj.subject_contains_all_files(subName)));
-                disp(['subject# ', char(num2str(i)), '|| Name: ', subName, ' || Contains all bdf results: ', is_valid]);
+                subjectName = char(obj.subject_names(i));
+                subjectlocation = char(obj.subject_locations(i));
+                is_valid = char(string(obj.subject_contains_all_files(subjectName, subjectlocation)));
+
+                disp(['subject# ', char(num2str(i)), '|| Location: ', subjectlocation,  '|| Name: ', subjectName, ' || Contains all bdf results: ', is_valid]);
             end
         end
     end
@@ -165,14 +209,26 @@ classdef EEG_quality_generator
         function subDirs = getSubDirectories(dirPath)
                 % Get a list of all files and folders in the given directory
                 allItems = dir(dirPath);
-                
                 % Keep only the items that are directories and exclude '.' and '..'
                 isSubDir = [allItems.isdir];  % Logical array for directories
                 subDirs = {allItems(isSubDir).name};  % Extract names of directories              
                 % Remove '.' and '..' from the list
                 subDirs = subDirs(~ismember(subDirs, {'.', '..'}));
-
-                
         end
+
+        function filename = find_bdf_by_pattern(dirname, pattern)
+            % looks for all the files that correspond to a pattern in a
+            % directory and return the first one (we could do the lowest
+            % one alphabetically)
+            filePattern = fullfile(dirname, pattern);
+            files = dir(filePattern);
+            if isempty(files)
+                filename = '';
+            else 
+                filename = [char(dirname), char(files(1).name)];
+            end  
+        end
+
     end
+
 end
--- EEG_toCSV_test.m
+++ EEG_toCSV_test.m
@@ -1,55 +1,35 @@
-% Example using EEG_quality_generator
-% Written by:  Antoine Jean (jean.75@osu.edu / antoine.jean.2@ulaval.ca), 2024-09-11
-
 clear;
 clc;
 
-% In order for the class to work properly as it is now, you should make sure to:
-% 
-% Point soarDataPath to the data directory that contains only the folders for
-% the individuals we want to analyse (each individual folder in that directory should contain all 3 properly named bdf files 
-% (each bdf file should be named: [subject_name]-[ERN|LST|NPU].bdf)). 
-% We can ofc change the way the class reads the files if they're arranged differently in the supercomputer.
-%
-% savedDataPath can point to anywhere just all the preproc data will be
-% sent somewhere in there
-%
-% binlisters_path should point to a directory that contain the 3 binlisters
-% (you normally have off the github), with the proper names (i might have renamed ErnBinlister.txt)
-% obj.binlisterPathErn = [binlisters_path, 'ErnBinlister.txt'];
-% obj.binlisterPathLst = [binlisters_path, 'LstBinlister.txt'];
-% obj.binlisterPathNpu = [binlisters_path, 'NpuBinlister.txt'];
-%
-% channelLocationFile just points to the channelLocationFile
-
-% The way i've set it up on my machine is all these 4 folders are in the
-% same level as the whole altaircode directory. Then I can just use the relative
-% path to access them. In theory we could all set this up like
-% this and not have to mess around with path variables too much. 
 soarDataPath = '..\soarData\';
 savedDataPath = '..\savedData';
 binlisters_path = '..\binlisters\';
 channelLocationFile = '..\EEGLAB_github_pull\eeglab\plugins\dipfit\standard_BESA\standard-10-5-cap385.elp';
+save_processed = true;
 
-% creates the path variables and reads the directory for subject names
-EEGs = EEG_quality_generator(soarDataPath, savedDataPath, binlisters_path, channelLocationFile);
+% creates the variables for saving data and reads the directory for subject
+% names
+EEGs = EEG_quality_generator(soarDataPath, savedDataPath, binlisters_path, channelLocationFile, save_processed);
 
-% list_subjects displays subject names in terminal, and whether or not for every subject
-% it could find all 3 necessary bdf files. Output in terminal should look like this:
+% displays subject names in terminal, and wether or not for every subject
+% it could find all 3 necessary bdf files
+
+% it should look like this:
 % subject# 1|| Name: OSU-TEST-01 || Contains all bdf results: true
 % subject# 2|| Name: OSU-TEST-02 || Contains all bdf results: true
 % subject# 3|| Name: OSU-TEST-03 || Contains all bdf results: true
 EEGs.list_subjects();
 
 
+
 % test_calculate_all_quality_scores simply fills the percentages with
 % arbitrary floats to simulate the real thing
 EEGs = EEGs.test_calculate_all_quality_scores();
 
-% calculate_all_quality_scores is the real thing and calls all 3 preproc
-% functions for all subjects. It takes me a good 5 minutes to run on my
-% machine.
+% calculate_all_quality_scores is the real thing and call all 3 preproc
+% functions for all subjects.
+% To try it, just make sure list_subjects outputs a list of subject
+% makes sense, and comment this line out: 
 % EEGs = EEGs.calculate_all_quality_scores();
 
-% This saves the data in a csv file in the same place as this .m file.
 EEGs.saveEEGtoCSV();
